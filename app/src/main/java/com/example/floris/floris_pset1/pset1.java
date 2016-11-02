9c67f8c128353affb2099a30ef3b7e5b842fc467
tartiflette1990 <floriskuipers@gmail.com>
2016-11-02 15:00:26 +0100

    Included landscape mode

--- app/src/main/java/com/example/floris/floris_pset1/pset1.java
+++ app/src/main/java/com/example/floris/floris_pset1/pset1.java
@@ -8,7 +8,7 @@ public class pset1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pset1);
+        setContentView(R.layout.landscapeview);
     }
 }
 
