1d7dbc62947879680a42de51f3a6fb3ce165cd35
tartiflette1990 <floriskuipers@gmail.com>
2016-11-02 10:43:59 +0100

    Initial commit

---
+++ app/src/main/java/com/example/floris/floris_pset1/pset1.java
@@ -0,0 +1,14 @@
+package com.example.floris.floris_pset1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pset1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pset1);
+    }
+}
+
